68b5f74419e702abbbbae23aee86144f20e8f551
AnvarAtayev <anvar936@live.com>
2024-10-19 11:00:11 +0100

    Add favicon and some draft articles

---
+++ content/posts/mathematics/20240805_introduction_to_lean.md
@@ -0,0 +1,6 @@
+---
+title: "Introduction to Lean"
+date: 2024-08-05T10:44:03+01:00
+draft: True
+---
+
---
+++ docs/js/katex-macros.js
@@ -0,0 +1,10 @@
+window.katexMacros = {
+    '\\RR': '\\mathbb{R}',
+    '\\ZZ': '\\mathbb{Z}',
+    '\\NN': '\\mathbb{N}',
+    '\\QQ': '\\mathbb{Q}',
+    '\\CC': '\\mathbb{C}',
+    '\\PP': '\\mathbb{P}',
+    '\\EE': '\\mathbb{E}',
+    // Add more macros as needed
+};
